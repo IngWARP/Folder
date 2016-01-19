69a2414ac9828849bde8b100d57b755b4d6bb5d1
Ingvar Kreft <ingvar.kreft@cybercom.com>
2016-01-19 13:02:59 +0100

    Added Vue

--- src/resources/views/folder.blade.php
+++ src/resources/views/folder.blade.php
@@ -3,8 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link href="{{ URL::asset('css/all.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('css/vendor/font-awesome.css') }}" rel="stylesheet" type="text/css">
+
+    <style>
+        body {
+            font-family: Menlo, Consolas, monospace;
+            color: #444;
+        }
+        .item {
+            cursor: pointer;
+        }
+        .bold {
+            font-weight: bold;
+        }
+        ul {
+            padding-left: 1em;
+            line-height: 1.5em;
+            list-style-type: dot;
+        }
+    </style>
+    <!-- item template -->
+    <script type="text/x-template" id="item-template">
+        <li>
+            <div
+                    :class="{bold: isFolder}"
+            @click="toggle"
+            @dblclick="changeType">
+            @{{model.name}}
+            <span v-if="isFolder"><i class="fa fa-folder-@{{open ? 'open-' : ''}}o"></i></span>
+            </div>
+            <ul v-show="open" v-if="isFolder">
+                <item
+                        class="item"
+                        v-for="model in model.children"
+                        :model="model">
+                </item>
+                <li @click="addChild">+</li>
+            </ul>
+        </li>
+    </script>
 </head>
 <body>
-hejsan
+<div class="container">
+    <div class="content">
+        <!-- the demo root element -->
+        <ul id="demo">
+            <item
+                    class="item"
+                    :model="treeData">
+            </item>
+            @{{ $data | json }}
+        </ul>
+    </div>
+</div>
+
+<script src="{{ URL::asset('js/vendor/vue/vue.js') }}"></script>
+<script src="{{ URL::asset('js/vendor/vue/vue-resource.js') }}"></script>
+<script src="{{ URL::asset('js/folder.js') }}"></script>
+
 </body>
 </html>
